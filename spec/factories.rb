e763367e6a88b50a0a09a330569426026dfd52a3
Visar Kastrati <vkastrati@hotmail.com>
2013-07-14 15:40:44 +0200

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name "Visar Kastrati"
-    email "vkastrati@hotmail.com"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
   end
